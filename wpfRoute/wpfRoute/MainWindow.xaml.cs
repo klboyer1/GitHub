6a34fab2255889e7ab8fec162cad541a1ad4b51f
klboyer1 <klboyer1@gmail.com>
2017-08-03 20:45:26 -0400

    first commit

---
+++ wpfRoute/wpfRoute/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+
+namespace wpfRoute
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteControl sss = new SQLiteControl();
+            string qry = "select * from route";
+            sss.ExecQuery(qry);
+            Grid1.ItemsSource =  sss.DT.DefaultView;
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ wpfRoute/wpfRoute/SQLControl.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+
+namespace wpfRoute
+{
+    class SQLiteControl
+    {
+        public String cs = "C:\\Users\\klboy\\GitHub\\wpfRoute\\wpfRoute\\AppData\\Route.db";
+
+        // QUERY STATISTICS
+        public int RecordCount;
+        public DataTable DT;
+
+        // EXECUTE QUERY SUB
+        public void ExecQuery(String Query)
+        {
+            // RESET QUERY STATS
+            RecordCount = 0;
+        
+            try
+            {
+
+                // CREATE DB COMMAND
+                SQLiteConnection conn = new SQLiteConnection(cs);
+                SQLiteCommand Cmd = new SQLiteCommand(Query, conn);
+                SQLiteDataAdapter DA = new SQLiteDataAdapter(Cmd);
+                DT = new DataTable();
+
+                // EXECUTE COMMAND & FILL DATASET
+                RecordCount = DA.Fill(DT);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}    
